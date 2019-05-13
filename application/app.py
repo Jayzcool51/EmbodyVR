bd1ab33bf5ba6e05e0f0ee3a15b1e8f179875cc4
jayzcool51 <jainamsheth09@gmail.com>
2019-05-12 17:20:28 -0700

    load balancer not working

--- application/app.py
+++ application/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask,request
 import os
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World! "+str(os.environ['MESSAGE'])
-
+users={"abc":123,"man":"mankey","dog":"dogkey","cat":"catkey"}
+@app.route('/user',methods=['GET'])
+def user():
+	try:
+		user=request.args.get('user')
+		return "response from "+str(os.environ['MESSAGE'])+" key for given user:"+str(user) 
+	except Exception as e:
+		return "400 Key Error"+str(e)
 if __name__ == "__main__":
     app.run(port=5000,host='0.0.0.0',debug=True)
